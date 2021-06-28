4b3511877f1c167a698bbd9020bc10abdfff9c75
Ashfahan <me@ashfahan.com>
2021-06-28 16:48:32 +0500

    load bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ console.clear();
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style/fluid.css";
 import "remixicon/fonts/remixicon.css";
+import "bootstrap/dist/js/bootstrap.bundle"
 import "./assets/Bebas-Neue/fonts.css";
 import "./assets/Poppins/fonts.css";
 import "./index.scss";
