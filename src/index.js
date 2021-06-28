7b7b29e4f23a4d2c66b990d5e0b5c5312514a22d
Ashfahan <me@ashfahan.com>
2021-06-28 17:42:10 +0500

    :heavy_plus_sign: Add 404 not found handle

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ console.clear();
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style/fluid.css";
 import "remixicon/fonts/remixicon.css";
+// import "@popperjs/core"
 import "bootstrap/dist/js/bootstrap.bundle"
 import "./assets/Bebas-Neue/fonts.css";
 import "./assets/Poppins/fonts.css";
@@ -9,4 +10,10 @@ import "./index.scss";
 
 // dymanicall import and add page
 const path = window.location.pathname !== "/" ? window.location.pathname : "/home";
-import(`./views${path}.js`).then((page) => page.load());
+console.log(window.location);
+import(`./views${path}.js`)
+  .then((page) => page.load())
+  .catch((error) => {
+    console.error(error);
+    import(`./views/notfound`).then((page) => page.load());
+  });
