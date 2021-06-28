69337e811fb07e54998c9ad13c46ea38e7b1e4c2
Ashfahan <me@ashfahan.com>
2021-06-28 15:01:08 +0500

    :heavy_plus_sign: Add homepage view

--- src/views/home.js
+++ src/views/home.js
@@ -1,3 +1,43 @@
-export const html = `<div>asd <div id="xxx">sss</div></div>`;
+const title = import("../assets/title.gif");
+const background = import("../assets/background.png");
 
-export const script = () => {};
+export const load = (...data) => {
+  const resource = [title, background];
+  Promise.all(resource).then((file) => {
+    file.forEach((file, index) => (resource[index].file = file.default));
+    document.body.innerHTML = html(...data);
+    script();
+  });
+};
+
+export const html = () => {
+  return `<header class="bg-hide fixed lyt-header grid pt-2">
+      <div class="column w-3 is-lg txt-bold">Ashfahan</div>
+      <div class="column off-5 w-16 justify-end align-middle flex">
+        <div class="flex justify-around">
+          <div class="btn is-min is-primary border-hide clr-black font-500">About</div>
+          <div class="btn is-min is-primary border-hide clr-black font-500">Contact</div>
+          <div class="btn is-min is-primary border-hide clr-black font-500">Product</div>
+          <div class="btn is-min is-primary border-hide clr-black font-500">Blog</div>
+        </div>
+        <div class="btn is-sld is-primary icon ri-lg p-20px is-round ml-2 inline-flex"><i class="icon ri-shopping-bag-3-fill"></i></div>
+        <div class="btn is-sld is-primary icon ri-lg p-20px is-round ml-2 inline-flex"><i class="icon ri-user-line"></i></div>
+      </div>
+    </header>
+    <main class="img-bg" style="background-position: unset; background-image: url(${background.file})">
+      <section class="relative p-4 h-min-100vh grid align-middle h-100">
+        <div class="column relative">
+          <div class="title is-sm txt-bold">We partner with <span class="txt-outline-primary clr-white">SaaS</span> and <span class="txt-outline-primary clr-white">digital product owners</span> to provide <span class="txt-outline-primary clr-white">design solutions</span> that scale for years to come.</div>
+          <div class="bg-light py-2 px-4 is-pill mt-2 flex">
+            <input type="search" placeholder="Enter Amazon Url" class="bg-hide input is-min" />
+            <button class="btn is-sld is-primary is-pill py-2 px-6 ml-4 flex">Search <i class="ml-6 icon ri-search-2-line"></i></button>
+          </div>
+        </div>
+        <div class="column relative txt-center"><img class="img" src="${title.file}" alt="" /></div>
+      </section>
+    </main>`;
+};
+
+export const script = () => {
+  console.log("done");
+};
